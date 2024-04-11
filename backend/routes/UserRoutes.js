3c7783eb02451d1ceaea3bea62250548490dee51
Caio Sá <caiobarcellos16@gmail.com>
2024-04-11 11:26:44 -0300

    Verificacao do token do usuario

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -3,10 +3,14 @@ const router = require('express').Router()
 const UserController = require('../controllers/UserController')
 const User = require('../models/User')
 
+// Middleware
+const verifyToken = require('../helpers/verify-token')
+
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
 router.get('/checkuser', UserController.checkUser)
 router.get('/:id', UserController.getUserById)
+router.patch('/edit/:id', verifyToken, UserController.editUser)
 
 
 
