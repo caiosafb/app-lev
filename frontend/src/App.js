d6f55950cdaa81e7e00f3caef8531ad794200440
Caio Sá <caiobarcellos16@gmail.com>
2024-05-13 16:17:46 -0300

    criacao componente de mensagem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+/* components */
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Container from './components/layout/Container';
+import Message from './components/layout/Message'
+
+/* pages */
 import Login from './components/pages/Auth/Login';
 import Register from './components/pages/Auth/Register';
 import Home from './components/pages/Home';
@@ -17,6 +22,7 @@ function App() {
         <Router>
              <UserProvider>
             <Navbar />
+            <Message/>
             <Routes>
                 <Route path='/login' element={<Container backgroundImage="login"><Login /></Container>} />
                 <Route path='/register' element={<Container backgroundImage="register"><Register /></Container>} />
